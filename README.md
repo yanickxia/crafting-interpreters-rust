02ffd8c5b4ecfab45e6d432029e5651a1a42b04f
xiayan.cn <xiayan.cn@bytedance.com>
2022-10-22 12:34:18 +0800

    add scanner

---
+++ README.md
@@ -0,0 +1,4 @@
+# crafting interpreters
+
+
+impl [crafting interpreters](http://www.craftinginterpreters.com/) by Rust
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use crafting_interpreters::runtime::Runtime;
+
+fn main() {
+    let mut runtime = Runtime::default();
+    let arg_length = std::env::args().count();
+    if arg_length > 2 {
+        println!("Usage: jlox [script]");
+        std::process::exit(64);
+    } else if arg_length == 2 {
+        runtime.run_file(std::env::args().nth(1).unwrap());
+    } else {
+        runtime.run_prompt();
+    }
+}
